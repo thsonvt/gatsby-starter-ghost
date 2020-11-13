5f07ee8a7998b2bdf573174658e95da9df06a0f3
Son Le <sonle@sg.ibm.com>
2020-11-13 19:07:52 +0800

    update siteUrl to hustlertalk.tech

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://hustlertalk-ghostblog.herokuapp.com/`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://hustlertalk.tech/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
