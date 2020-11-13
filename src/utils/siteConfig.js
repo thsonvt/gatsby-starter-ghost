ab5e97b29fe5e25e455c48ffca02326f8b39a384
Son Le <sonle@sg.ibm.com>
2020-11-13 19:14:02 +0800

    change site name and description

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Hustler Talk`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Conversations with hustlers in startup and tech`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
